902b64e2587a1ff05cf42100ed574cd4bed85c2f
Kangjie <1365595476@qq.com>
2019-04-06 20:43:09 +0800

    add swiper

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl"/>
+      </swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [{
+        id: '0001',
+        imgUrl: 'https://source.qunarzz.com/site/images/wns/20190402_qunar_dujia_homepage_3.jpg'
+      }, {
+        id: '0002',
+        imgUrl: 'https://source.qunarzz.com/site/images/wns/20190328_qunar_dujia_homepage_2.jpg'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .wrapper >>> .swiper-pagination
+    span:first-child
+      margin-left 0 !important
+  .wrapper >>> .swiper-pagination-bullet-active
+    background #fff
+  .wrapper
+    overflow hidden
+    width 100%
+    height 0
+    padding-bottom 25.6%
+    background #eee
+    .swiper-img
+      width 100%
+</style>
