21b3d3f48bd99e0f94d2ca9e9c9e97c06d3ffcf3
Kangjie <1365595476@qq.com>
2019-04-10 12:34:09 +0800

    add detaillist

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -3,6 +3,9 @@
     <div class="content">
       <detail-banner></detail-banner>
       <detail-header></detail-header>
+      <div class="content">
+        <detail-list :list="list"></detail-list>
+      </div>
     </div>
   </div>
 </template>
@@ -10,11 +13,44 @@
 <script>
 import DetailBanner from './commponents/Banner'
 import DetailHeader from './commponents/Header'
+import DetailList from './commponents/List'
+import axios from 'axios'
 export default {
   name: 'Detail',
   components: {
     DetailBanner,
-    DetailHeader
+    DetailHeader,
+    DetailList
+  },
+  data () {
+    return {
+      sightName: '',
+      bannerImg: '',
+      gallaryImgs: [],
+      list: []
+    }
+  },
+  methods: {
+    getDetailInfo () {
+      axios.get('/api/detail.json', {
+        params: {
+          id: this.$route.params.id
+        }
+      }).then(this.handleGetDataSucc)
+    },
+    handleGetDataSucc (res) {
+      res = res.data
+      if (res.ret && res.data) {
+        const data = res.data
+        this.sightName = data.sightName
+        this.bannerImg = data.bannerImg
+        this.gallaryImgs = data.gallaryImgs
+        this.list = data.categoryList
+      }
+    }
+  },
+  mounted () {
+    this.getDetailInfo()
   }
 }
 </script>
